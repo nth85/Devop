c37543253af06aa86ffdb76b218b4989d009a392
nthuy <huynt185@gmail.com>
2025-03-24 21:35:33 +0700

    update 24.03

--- linux.sh
+++ linux.sh
@@ -1,5 +1,8 @@
-##nslookup phân giải các địa chỉ ip
+**nslookup phân giải các địa chỉ ip**
+```
 nslookup 24h.com.vn
-## curl noproxy 
+```
+**curl noproxy**
+``` 
 curl --noproxy '*' https://dantri.com.vn
-## 
+```
